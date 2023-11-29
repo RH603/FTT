0c90950082db983dda714beb544eac94f0c0f8c2
RH603 <gksgh123@naver.com>
2023-11-29 17:29:07 +0900

    push

---
+++ index/JS/modal.js
@@ -0,0 +1,22 @@
+const modalImgBack = document.querySelector(".modal-img-back");
+const modalImg = document.querySelector(".modal-img-win");
+const images = document.querySelectorAll("img");
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", () => {
+        console.log(images[i].src);
+
+        // Create a new image element
+        const clickedImage = document.createElement("img");
+        clickedImage.src = images[i].src;
+
+        // Clear the modal content before appending a new image
+        modalImg.innerHTML = "";
+
+        // Append the clicked image to the modal
+        modalImg.appendChild(clickedImage).classList.add("modal-img");
+
+        // Display the modal
+        modalImgBack.style.display = "block";
+    });
+}
