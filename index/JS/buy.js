989739d4b28cd200441c03a4b412ad295fae57ee
jeonsunam <144972537+jeonsunam@users.noreply.github.com>
2023-12-01 10:30:50 +0900

    new

--- index/JS/buy.js
+++ index/JS/buy.js
@@ -1,3 +1,22 @@
+async function getPay() {
+  const snapshot = await getData("FTT", "FTT-Pay");
+  const { payname, payquality, payuse, paywatch } = snapshot.data();
+
+  content.insertAdjacentHTML(
+    "beforebegin",
+    `
+
+    <div style="padding:10px 16px">
+  <p><h1 style="margin-bottom:1rem;">${payname}</h1></p>
+  <p style="margin-bottom:1rem;">${payquality}</p>
+  <p>${payuse}</p>
+  <p>${paywatch}</p>
+  </div>
+
+  `
+  );
+}
+
 function buyPopup() {
-  window.open("buy.html", "popup", "width=750,height=700");
+  window.open("buy-ex.html", "popup", "width=750,height=700");
 }
