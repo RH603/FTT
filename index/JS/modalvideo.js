cf3dda12796479860a5197d41a05dd5139b3cd2e
RH603 <gksgh123@naver.com>
2023-12-04 12:30:02 +0900

    push

--- index/JS/modalvideo.js
+++ index/JS/modalvideo.js
@@ -6,6 +6,8 @@ const modalVid = document.querySelector(".movie-container")
 const htmlImgs = document.querySelectorAll(".htmlImg")
 const kateBox = document.querySelector(".kate-box")
 
+const pTag = document.createElement("p")
+const textNode = document.createTextNode("Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum suscipit corrupti doloremque aperiam, tenetur sapiente sunt ratione officia quidem necessitatibus, numquam voluptatem blanditiis delectus distinctio nulla sed nisi porro.");
 
 for (let i = 0; i < images.length; i++) {
     images[i].addEventListener("click", () => {
@@ -21,11 +23,12 @@ for (let i = 0; i < images.length; i++) {
         clickedImage.src = video[i].src;
         // 추가
         clickedImg.src = images[i].src;
-        clickedHtml.src = shuffledhtmlUrls[i]   
+        // clickedHtml.src = shuffledhtmlUrls[i]
         // 
 
         clickedVideo.classList.add("modal-video");
         clickedImage.autoplay = true
+        clickedImage.controls = true
         // Clear the modal content before appending a new image
         modalImg.innerHTML = "";
         
@@ -35,11 +38,13 @@ for (let i = 0; i < images.length; i++) {
         // modalImg.appendChild(clickedImg)
 
         modalImg.appendChild(clickedHtml)
+        modalImg.appendChild(pTag)
+        pTag.appendChild(textNode)
         
         // Display the modal
         modalImgBack.style.display = "block";
         modalImg.style.display = "block"
-        document.querySelector(".htmlImg").style.display= "block"
+        // document.querySelector(".htmlImg").style.display= "block"
         document.querySelector("video").style.display= "block"
 
 
@@ -54,7 +59,7 @@ for (let i = 0; i < images.length; i++) {
                 // modalImgBack.forEach((img) => {
                     // });         
                 }else{
-                    document.querySelector("video").style.display= "none"
+                    document.querySelector("video").style.display= "block"
                     // clickedHtml.parentNode.removeChild(clickedHtml);
                     // document.querySelector(".htmlImg").style.display = "none"
                     // modalImg.appendChild(clickedHtml);
