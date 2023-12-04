9994eee4a1b70ac36370d966e959a511eca54ab2
junjunhee12 <44117412+junjunhee12@users.noreply.github.com>
2023-12-04 09:23:21 +0900

    new

--- index/JS/modal.js
+++ index/JS/modal.js
@@ -1,26 +1,25 @@
 const modalImgBack = document.querySelector(".modal-img-back");
 const modalImg = document.querySelector(".modal-img-win");
 const images = document.querySelectorAll("img");
-
 for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", () => {
-        alert("click")
-        // Create a new image element
-        const clickedImg = document.createElement("img");
-        clickedImg.src = images[i].src;
-        console.log(images[i].src);
+  images[i].addEventListener("click", () => {
+    alert("click");
+    // Create a new image element
+    const clickedImg = document.createElement("img");
+    clickedImg.src = images[i].src;
+    console.log(images[i].src);
 
-        // Clear the modal content before appending a new image
-        modalImg.innerHTML = "";
+    // Clear the modal content before appending a new image
+    modalImg.innerHTML = "";
 
-        // Append the clicked image to the modal
-        modalImg.appendChild(clickedImg).classList.add("modal-img");
+    // Append the clicked image to the modal
+    modalImg.appendChild(clickedImg).classList.add("modal-img");
 
-        // Display the modal
-        modalImgBack.style.display = "block";
-    });
+    // Display the modal
+    modalImgBack.style.display = "block";
+  });
 }
 
-modalImgBack.addEventListener("click",()=>{
-    modalImgBack.style.display = "none";
-})
+modalImgBack.addEventListener("click", () => {
+  modalImgBack.style.display = "none";
+});
