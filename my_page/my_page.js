2dc88fe17b70c3c0387e41dd895489ea9f5b929c
keh10 <slslsl0322@naver.com>
2023-12-05 09:32:01 +0900

    push

---
+++ my_page/my_page.js
@@ -0,0 +1,42 @@
+const alarm = document.querySelector(".alarm");
+const alarmId = document.querySelector("#alarmId");
+const modalMask = document.querySelector(".modal-mask");
+const popupCloseIr = document.querySelector(".popup-close");
+const setting = document.querySelectorAll(".set");
+const option = document.querySelector(".option");
+const settiongInfo = document.querySelector(".setting-info");
+const settiongDel = document.querySelector(".setting-del");
+const allWin = document.querySelector("body");
+console.log(popupCloseIr);
+// console.log("hello");
+// console.log(alarmId);
+
+alarm.addEventListener("click", () => {
+  // alert("click");
+  modalMask.style.display = "block";
+});
+
+popupCloseIr.addEventListener("click", () => {
+  modalMask.style.display = "none";
+});
+
+// option.addEventListener("click", () => {
+//   setting[0].style.display = "block";
+//   setting[1].style.display = "block";
+// });
+settiongInfo.addEventListener("click", () => {
+  alert("click");
+});
+settiongDel.addEventListener("click", () => {
+  alert("click");
+});
+
+allWin.addEventListener("click", (event) => {
+  if (event.target === option) {
+    setting[0].style.display = "block";
+    setting[1].style.display = "block";
+  } else {
+    setting[0].style.display = "none";
+    setting[1].style.display = "none";
+  }
+});
