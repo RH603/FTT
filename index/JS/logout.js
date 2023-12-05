adef2552e5b363bdbfbccf70e80ed0059e439781
jeonsunam <144972537+jeonsunam@users.noreply.github.com>
2023-12-05 09:45:31 +0900

    new

--- index/JS/logout.js
+++ index/JS/logout.js
@@ -3,7 +3,7 @@ const logout = document.querySelector(".g-main .logOut");
 logout.addEventListener("click", function () {
   const out = confirm("로그아웃 하시겠습니까?");
   if (out == true) {
-    // location.href = "" 처음페이지이동
+    location.href = "http://127.0.0.1:5501/index/HTML/page.html";
   } else {
     alert("감사합니다.");
   }
